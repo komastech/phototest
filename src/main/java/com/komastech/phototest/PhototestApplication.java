aad242f0db1841c6a0ad7a238886a0692618bc4a
Loic Messy <loiccarrel.messy@gmail.com>
2024-09-24 10:11:13 +0200

    initialisation

---
+++ src/main/java/com/komastech/phototest/PhototestApplication.java
@@ -0,0 +1,13 @@
+package com.komastech.phototest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhototestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhototestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/komastech/phototest/PhototestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.komastech.phototest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhototestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
